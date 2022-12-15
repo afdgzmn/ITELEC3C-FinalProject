7662aa5b8e968b4c2775f1266ac77b9d6d820af3
Kean Cabel <72328304+kn-cbl@users.noreply.github.com>
2022-12-15 22:45:03 +0800

    Updated css

--- signup.php
+++ signup.php
@@ -27,7 +27,7 @@ if (isset($_POST['btn_signup'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/signin.css">
+	<link rel="stylesheet" href="css/auth.css">
 	<script src="js/signup.js" defer></script>
 </head>
 
